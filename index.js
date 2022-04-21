29cd9592833e7d8af988a918a990d51ced55049d
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-21 12:09:59 +0600

    users added

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Bro I am Using node js yeah!!!");
+  res.send("Yeah This Is working");
 });
-
+const users = [
+  { id: 1, name: "akib", gmail: "akib@gmail.com" },
+  { id: 2, name: "sabana", gmail: "sabana@gmail.com" },
+  { id: 3, name: "lolita", gmail: "lolita@gmail.com" },
+  { id: 4, name: "sabnor", gmail: "sabnor@gmail.com" },
+  { id: 5, name: "rajjak", gmail: "rajjak@gmail.com" },
+  { id: 6, name: "sharok", gmail: "sharok@gmail.com" },
+  { id: 7, name: "bobita", gmail: "bobita@gmail.com" },
+];
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.get("/user/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.send(user);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
